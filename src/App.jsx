40fbbbcb6fb6d71953980281627777e9367465c6
ishworX <git.ishwor@gmail.com>
2024-02-18 16:54:56 -0700

    Navbar initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Navbar } from "./components/Navbar/Navbar";
 
 const App = () => {
-  return <div>
-    
-    <h1>Hello World</h1>
-    
-    </div>;
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
 };
 
 export default App;
