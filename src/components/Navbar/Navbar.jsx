a73b4e2b579ed2b6df6722478941e8b4e122737d
ishworX <git.ishwor@gmail.com>
2024-02-19 23:44:15 -0700

    completed Nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,21 +3,32 @@
 //  Logo works best with White background.
 // Swithing to dark theme might make the text disapper!
 import React from 'react'
-import Logo from "../../assets/newTaste.png"
+import Logo from "../../assets/newTaste-logo.png"
 
 export const Navbar = () => {
     return <>
     <div className='shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200'>
         <div className='container'>
-            <div className="flex justify-center items-center">
+            <div className="flex justify-between items-center ">
             <div>
-            <a href='#'>
+            <a href='#' className='flex items-center gap-2 text-2xl sm:text-2xl font-bold'>
             <img src={Logo} 
             alt='NewTasteOfIndia'
             className='w-10'/>
             New Taste Of India
             </a>
             </div>
+            <ul className='flex gap-4'>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+            </ul>
 
             </div>
         </div>
