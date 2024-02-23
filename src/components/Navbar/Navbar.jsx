f6a9e4786480052d411fa174f047cfd9667e0732
ishworX <git.ishwor@gmail.com>
2024-02-22 23:55:00 -0700

    tried using functions

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,34 +7,35 @@ import Logo from "../../assets/newTaste-logo.png"
 
 export const Navbar = () => {
     return <>
-    <div className='shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200'>
-        <div className='container'>
-            <div className="flex justify-between items-center ">
-            <div>
-            <a href='#' className='flex items-center gap-2 text-2xl sm:text-2xl font-bold'>
-            <img src={Logo} 
-            alt='NewTasteOfIndia'
-            className='w-10'/>
-            New Taste Of India
-            </a>
-            </div>
-            <ul className='hidden sm:flex gap-4'>
-                <li>
-                    <a href="#" className='inline-block py-4 px-4 hover:text-primary'>Home</a>
-                </li>
-                <li>
-                    <a href="#" className='inline-block py-4 px-4 hover:text-primary'>About</a>
-                </li>
-                <li>
-                    <a href="#" className='inline-block py-4 px-4 hover:text-primary'>Contact</a>
-                </li>
-            </ul>
-            <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3">
-                Order
-            </button>
+        <div className='shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200'>
+            <div className='container'>
+                <div className="flex justify-between items-center ">
+                    <div>
+                        <a href='#' className='flex items-center gap-2 text-2xl sm:text-2xl font-bold'>
+                            <img src={Logo}
+                                alt='NewTasteOfIndia'
+                                className='w-10' />
+                            New Taste Of India
+                        </a>
+                    </div>
+                    <ul className="hidden sm:flex items-center gap-4">
+                        {Menu.map((menu) => (
+                            <li key={menu.id}>
+                                <a
+                                    href={menu.link}
+                                    className="inline-block py-4 px-4 hover:text-yellow-500"
+                                >
+                                    {menu.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                    <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3">
+                        Order
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
     </>;
-    
+
 }
