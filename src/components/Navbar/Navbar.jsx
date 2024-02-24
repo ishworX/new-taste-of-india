808e799271015b4732c7da6dd95578891fa366c8
ishworX <git.ishwor@gmail.com>
2024-02-23 23:52:42 -0700

    error that needs to be fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,17 +4,15 @@
 // Swithing to dark theme might make the text disapper!
 import React from 'react'
 import Logo from "../../assets/newTaste-logo.png"
-import { FaCartShopping } from "react-icons/fa6";   
+import { FaCartShopping } from "react-icons/fa6";
 export const Navbar = () => {
     return <>
-        <div className='shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200'>
-            <div className='container'>
-                <div className="flex justify-between items-center ">
+        <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
+            <div className="container py-3 sm:py-0">
+                <div className="flex justify-between items-center">
                     <div>
-                        <a href='#' className='flex items-center gap-2 text-2xl sm:text-2xl font-bold'>
-                            <img src={Logo}
-                                alt='NewTasteOfIndia'
-                                className='w-10' />
+                        <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+                            <img src={Logo} alt="Logo" className="w-10" />
                             New Taste Of India
                         </a>
                     </div>
@@ -33,7 +31,6 @@ export const Navbar = () => {
                     <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3">
                         Order
                         <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
-
                     </button>
                 </div>
             </div>
