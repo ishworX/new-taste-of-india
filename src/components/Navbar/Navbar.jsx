708f8c257e641d4ac0704bb9cf515ddc0dd3f8aa
ishworX <git.ishwor@gmail.com>
2024-02-26 23:09:23 -0700

    starting from beginning to check?

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,13 +6,7 @@ const Navbar = () => {
         <>
         <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
             <div className="container py-3 sm:py-0">
-                <div className="flex justify-between items-center">
-                    <div href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
-                    <img src={Logo} alt="Logo" className="w-10" /> 
-                    New Taste Of India
-
-                    </div>
-                </div>
+                Hello
             </div>
         </div>
         </>
