24daf612bd3c9badf59378daa93ce1a6a49a66b8
ishworX <git.ishwor@gmail.com>
2024-03-03 23:04:25 -0700

    making those image into Array so that it will be easy to call

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 import HeroImage from "../../assets/biryani3.png";
 import HeroImage2 from "../../assets/biryani5.png";
 import HeroImage3 from "../../assets/biryani2.png";
-import Vector from "../../assets/vector3.png";
+
+// import Vector from "../../assets/vector3.png";
+
+
+const ImageList = [ 
+    {
+        id : 1,
+        img: HeroImage,
+    },
+    {
+        id : 2,
+        img : HeroImage2,  
+    },
+    {
+        id : 3,
+        img : HeroImage3,
+    },
+];
+
+
 
 
 const Hero = () => {
