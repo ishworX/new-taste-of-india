255362e0493dd2c8f2fb1e728b0c12f0b97685bc
ishworX <git.ishwor@gmail.com>
2024-03-03 22:02:39 -0700

    getting images for the hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import HeroImage from "../../assets/biryani3.png";
+import HeroImage2 from "../../assets/biryani5.png";
+import HeroImage3 from "../../assets/biryani2.png";
+import Vector from "../../assets/vector3.png";
+
 
 const Hero = () => {
   return (
     <div>
         <div>
-            
+
         </div>
     </div>
   )
