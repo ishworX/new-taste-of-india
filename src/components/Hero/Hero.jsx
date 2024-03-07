e61f80eaedd9403dea47f20f349b5f6d0181e138
ishworX <git.ishwor@gmail.com>
2024-03-06 23:53:21 -0700

    fixed the error but its not displaying anything

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import BiryaniImg1 from "../../assets/biryani3.png";
 import BiryaniImg2 from "../../assets/biryani5.png";
 import BiryaniImg3 from "../../assets/biryani2.png";
-import Vector from "../../assets/vector3.png";
+// import Vector from "../../assets/vector3.png";
 
 const ImageList = [
     {
