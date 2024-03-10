c9260a3fca47632d814663b9996f63da9bc4a11c
ishworX <git.ishwor@gmail.com>
2024-03-09 23:48:28 -0700

    uploaded images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import BiryaniImg1 from "../../assets/biryani3.png";
+import BiryaniImg2 from "../../assets/biryani5.png";
+import BiryaniImg3 from "../../assets/biryani2.png";
+import Vector from "../../assets/vector3.png";
+
+
 
 const Hero = () => {
     return (
         <>
-            <div> Hero Section </div>
+            <div className='text-2xl text-green-400'>
+                Is the Hero section working?
+            </div>
         </>
 
     )
