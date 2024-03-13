f50732a632f437aba7032a7306afc537bddcc14f
ishworX <git.ishwor@gmail.com>
2024-03-12 22:45:46 -0600

    basic skeleton

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+// things need to be done
+// About the restaurant
+// Services they provide
+
 const About = () => {
     return (
         <div>Testing if it worked properly?
