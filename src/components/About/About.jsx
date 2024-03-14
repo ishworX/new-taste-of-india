409412d3f746f2f3efeaa519e302cdf71a444e24
ishworX <git.ishwor@gmail.com>
2024-03-13 23:50:40 -0600

    action plan

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 // things need to be done
 // About the restaurant
 // Services they provide
+// Services would siund better 
+
+// need to get pics for restaurant
 
 const About = () => {
     return (
