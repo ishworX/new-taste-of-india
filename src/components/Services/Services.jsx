c54b67589109cc1cbe7731ca7b593e295a5bd49c
ishworX <git.ishwor@gmail.com>
2024-03-20 23:52:16 -0600

    wrote more about the about the restaursnt section

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -7,6 +7,11 @@ const Services = () => {
         <p>
             In a world that is increasingly coming closer, we seek to promote the harmony of Indian culture, cuisine and architecture. Best recipes and awesome spices are blended together in perfect ratio to give healthy meals to each and every one who come to us with the taste that lasts forever.
         </p>
+        <ul>
+            <li> Serving High Quality Multi-cuisines</li>
+            <li> Personalised Friendly Service </li>
+            <li> Exclusive Catering facilities</li>
+        </ul>
         </>
     )
 }
