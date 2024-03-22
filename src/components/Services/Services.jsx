1375894bfd91fb48401091078f359436628af21c
ishworX <git.ishwor@gmail.com>
2024-03-21 23:49:39 -0600

    testing the icons

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import BiryaniImg from "../../assets/biryani5.png";
+import Vector from "../../assets/vector3.png";
+import { GrSecure } from "react-icons/gr";
+import { IoFastFood } from "react-icons/io5";
+import { GiFoodTruck } from "react-icons/gi";
 
 const Services = () => {
     return (
