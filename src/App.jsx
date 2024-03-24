4cf52f004a779f5578834f2c3b3549fb2cd17356
ishworX <git.ishwor@gmail.com>
2024-03-23 23:30:44 -0600

    Fixed the error by commenting the unused icons on the Services sections

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
     <Navbar/>
     <Hero/>
     <About/>
+    <Services/>
     </>
   )
 };
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import BiryaniImg from "../../assets/biryani5.png";
+// import BiryaniImg from "../../assets/biryani5.png";
 import Vector from "../../assets/vector3.png";
-import { GrSecure } from "react-icons/gr";
-import { IoFastFood } from "react-icons/io5";
-import { GiFoodTruck } from "react-icons/gi";
+// import { GrSecure } from "react-icons/gr";
+// import { IoFastFood } from "react-icons/io5";
+// import { GiFoodTruck } from "react-icons/gi";
 
 const Services = () => {
     const bgImage = {
