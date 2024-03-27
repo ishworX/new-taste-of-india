d43dc9c10060c26351560d2983ee3ab72d73cfb2
ishworX <git.ishwor@gmail.com>
2024-03-26 23:25:35 -0600

    error is fixed but doesnot display on Web

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-// import BiryaniImg from "../../assets/biryani5.png";
+import React from "react";
+import BiryaniImg from "../../assets/biryani5.png";
 import Vector from "../../assets/vector3.png";
 // import { GrSecure } from "react-icons/gr";
 // import { IoFastFood } from "react-icons/io5";
@@ -14,7 +14,6 @@ const Services = () => {
         height: "100%",
         width: "100%",
     };
-
     return (
         <>
             <div className="min-h-[550px]">
@@ -33,44 +32,45 @@ const Services = () => {
                                     className="max-w-[430px] w-full mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)]"
                                 />
                             </div>
+                            {/* text content section */}
                             <div className="flex flex-col justify-center gap-6 sm:pt-0">
                                 <h1 className="text-3xl sm:text-4xl font-bold">
-                                    About the Restaurant
+                                    Lorem, ipsum dolor.
                                 </h1>
                                 <p className="text-sm text-gray-500 tracking-wide leading-5">
-                                    In a world that is increasingly coming closer, we seek to promote the harmony of Indian culture, cuisine and architecture. Best recipes and awesome spices are blended together in perfect ratio to give healthy meals to each and every one who come to us with the taste that lasts forever.
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                                    Eaque reiciendis inventore iste ratione ex alias quis magni at
+                                    optio
                                     <br />
                                     <br />
-                                    <ul>
-                                        <li> Serving High Quality Multi-cuisines</li>
-                                        <li> Personalised Friendly Service </li>
-                                        <li> Exclusive Catering facilities</li>
-                                    </ul>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Beatae ab sed, exercitationem minima aliquid eligendi
+                                    distinctio? Fugit repudiandae numquam hic quo recusandae.
+                                    Excepturi totam ad nam velit quasi quidem aspernatur.
                                 </p>
-                            </div>
-                            <div className="flex gap-6">
-                                <div>
-                                    <GrSecure className="text-4xl h-20 w-20 shadow-sm p-5 rounded-full bg-violet-100 dark:bg-violet-400" />
+                                <div className="flex gap-6">
+                                    <div>
+                                        <GrSecure className="text-4xl h-20 w-20 shadow-sm p-5 rounded-full bg-violet-100 dark:bg-violet-400" />
+                                    </div>
+                                    <div>
+                                        <IoFastFood className="text-4xl h-20 w-20 shadow-sm p-5 rounded-full bg-orange-100 dark:bg-orange-400" />
+                                    </div>
+                                    <div>
+                                        <GiFoodTruck className="text-4xl h-20 w-20 shadow-sm p-5 rounded-full bg-green-100 dark:bg-green-400" />
+                                    </div>
                                 </div>
                                 <div>
-                                    <IoFastFood className="text-4xl h-20 w-20 shadow-sm p-5 rounded-full bg-orange-100 dark:bg-orange-400" />
+                                    <button className="bg-gradient-to-r from-primary to-secondary text-white py-2 px-4 rounded-full shadow-xl hover:shadow-md">
+                                        Order Now
+                                    </button>
                                 </div>
-                                <div>
-                                    <GiFoodTruck className="text-4xl h-20 w-20 shadow-sm p-5 rounded-full bg-green-100 dark:bg-green-400" />
-                                </div>
-                            </div>
-                            <div>
-                                <button className="bg-gradient-to-r from-primary to-secondary text-white py-2 px-4 rounded-full shadow-xl hover:shadow-md">
-                                    Order Now
-                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div >
-    </>
-  );
+        </>
+    );
 };
 
 export default Services;
