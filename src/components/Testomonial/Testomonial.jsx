891b2bad4301cfe7750475c89d52a2240a9ac0b1
ishworX <git.ishwor@gmail.com>
2024-04-01 23:51:54 -0600

    testomonials

--- src/components/Testomonial/Testomonial.jsx
+++ src/components/Testomonial/Testomonial.jsx
@@ -1,5 +1,27 @@
 import React from 'react'
 
+const testimonialData = [
+  {
+    id: 1,
+    name: "Samuel",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/101/101",
+  },
+  {
+    id: 1,
+    name: "John Doe",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/102/102",
+  },
+  {
+    id: 1,
+    name: "Smith",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/103/103",
+  },
+];
+
+
 const Testomonial = () => {
   return (
     <div>Testomonial</div>
