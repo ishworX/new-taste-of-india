08e110431188358d13fa5af759d5d64176dd8f0b
ishworX <git.ishwor@gmail.com>
2024-04-03 23:26:15 -0600

    error with not displaying on windows

--- src/components/Testomonial/Testomonial.jsx
+++ src/components/Testomonial/Testomonial.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Slider from "react-slick";
+import Slider from "react-slick";
 
 const testimonialData = [
   {
@@ -87,4 +87,4 @@ const Testomonial = () => {
   );
 };
 
-export default Testimonial;
+export default Testomonial;
