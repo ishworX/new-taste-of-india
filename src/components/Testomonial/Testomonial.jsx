f7f5c5c9cce1d9a366fa5589eae9c65ae823e6f3
ishworX <git.ishwor@gmail.com>
2024-04-04 23:45:46 -0600

    need to fix?

--- src/components/Testomonial/Testomonial.jsx
+++ src/components/Testomonial/Testomonial.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import Slider from "react-slick";
+// import { Slider } from "react-slick";
 
 const testimonialData = [
   {
     id: 1,
-    name: "Samuel",
+    name: "Brent Fra",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
     img: "https://picsum.photos/101/101",
   },
   {
     id: 1,
-    name: "John Doe",
+    name: "Nakul Patel",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
     img: "https://picsum.photos/102/102",
   },
   {
     id: 1,
-    name: "Smith",
+    name: "Sarah H",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
     img: "https://picsum.photos/103/103",
   },
