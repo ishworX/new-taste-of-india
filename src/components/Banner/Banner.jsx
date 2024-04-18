90a5bd8653127b1e1933620241ce5cfcf875c983
ishworX <git.ishwor@gmail.com>
2024-04-17 23:35:41 -0600

    fixed spelling mistakes

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -52,5 +52,5 @@ export default CoverBanner;
 
 
 // Need to make promotional banner
-// Also need ot make a Order now buttom
-// Make the button perform call when the user clisk the order
+// Also need ot make a Order now button
+// Make the button perform call when the user click the order
